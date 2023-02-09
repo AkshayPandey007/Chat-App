181fde5fd2ada79d8db8b2b54b9e8fda5ddabf87
AkshayPandey007 <101665005+AkshayPandey007@users.noreply.github.com>
2023-02-09 22:00:48 +0530

    register

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "../CSS/Register.module.css"
 import chatLogo from "../Assest/chatLogo.jpg"
 import Slay from "../Assest/Saly-10.png"
 import { Link } from 'react-router-dom'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+
+
+  const toastCss = {
+    position: "bottom-center",
+    autoClose: 5000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+
+  const handleValidation = () => {
+    const { password, confirmPassword, username, email } = values;
+    if (password !== confirmPassword) {
+      toast.error( "Password and confirm password should be same.", toastCss);
+      return false;
+    } 
+    else if (username.length < 3) {
+      toast.error("Username should be greater than 3 characters.",toastCss);
+      return false;
+    } 
+    else if (password.length < 8) {
+      toast.error("Password should be equal or greater than 8 characters.", toastCss);
+      return false;
+    } 
+    else if (email === "") {
+      toast.error("Email is required.", toastCss);
+      return false;
+    }
+
+    return true;
+  };
+
+
+
+
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    handleValidation()
+    // console.log(values)
+  }
+
+  // console.log(values)
   return (
     <div className={styles.RegisterBox}>
 
@@ -15,25 +73,29 @@ const Register = () => {
       <img src={chatLogo} alt="" className={styles.Logo}/>
       </div>
 
-      <input type="text" placeholder='Enter Your Name' required/>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+
+      <input type="text" placeholder='Enter Your Name' required name="username" onChange={(e) => handleChange(e)}/>
 
       <br/>
 
-      <input type="email" placeholder='Enter Your Email' required/>
+      <input type="email" placeholder='Enter Your Email' required name="email" onChange={(e) => handleChange(e)}/>
 
       <br/>
       
-      <input type="password" placeholder='Create Password' required/>
+      <input type="password" placeholder='Create Password' required name="password" onChange={(e) => handleChange(e)}/>
 
       <br/>
 
-      <input type="password" placeholder='Confirm Password'  required />
+      <input type="password" placeholder='Confirm Password'  required  name="confirmPassword" onChange={(e) => handleChange(e)}/>
 
 
       <br/>
 
-      <button>REGISTER</button>
+      <button type='Submit'>REGISTER</button>
 
+
+      </form>
 
 
       <span className={styles.Alredy}>
@@ -48,6 +110,7 @@ const Register = () => {
       <div className={styles.rightBox}>
           <img src={Slay} alt="" />
       </div>
+      <ToastContainer/>
     </div>
   )
 }
